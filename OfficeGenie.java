981289de57fb30fdb89d83a41a5d992b7ed06ecc
Aiswarya Ravichandran <Aiswarya Ravichandran>
2016-02-15 15:22:51 -0600

    changes

--- OfficeGenie.java
+++ OfficeGenie.java
@@ -1,5 +1,6 @@
 public class OfficeGenie {
 	public static void main(String Args[]) {
 		System.out.println("Welcome to OfficeGenie System");
+		System.out.println("-- Unique product for unique people --");
 	}
 }
