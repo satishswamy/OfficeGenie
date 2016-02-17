a75e9dd4cff470c9fa037b8a69adc9b9827028cb
satishswamy <satishsswamy@hotmail.com>
2016-02-17 10:26:51 -0600

    new

--- OfficeGenie.java
+++
@@ -1,6 +0,0 @@
-public class OfficeGenie {
-	public static void main(String Args[]) {
-		System.out.println("Welcome to OfficeGenie System");
-		System.out.println("-- Unique product for unique people --");
-	}
-}
